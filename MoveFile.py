0c72847261f345151eb58afa5adeb7e391eadbd9
LilKwanVu <you@example.com>
2021-05-20 14:16:23 +0700

    Move File

--- MoveFile.py
+++ MoveFile.py
@@ -2,25 +2,34 @@ import shutil
 import os
 import glob
 
-source_below = 'D:/TFODCourse/HGM-4/HGM-1.0/Below_CAM/A'
-source_left =  'D:/TFODCourse/HGM-4/HGM-1.0/Left_CAM/A'
-source_right = 'D:/TFODCourse/HGM-4/HGM-1.0/Right_CAM/A'
-source_front = 'D:/TFODCourse/HGM-4/HGM-1.0/Front_CAM/A'
+source_below = 'C:/Users/Admin/Downloads/jzy8zngkbg-8/HGM-4/HGM-1.0/Below_CAM'
+source_left = 'C:/Users/Admin/Downloads/jzy8zngkbg-8/HGM-4/HGM-1.0/Left_CAM'
+source_right = 'C:/Users/Admin/Downloads/jzy8zngkbg-8/HGM-4/HGM-1.0/Right_CAM'
+source_front = 'C:/Users/Admin/Downloads/jzy8zngkbg-8/HGM-4/HGM-1.0/Front_CAM'
+
 dest_test = 'D:/TFODCourse/Tensorflow/workspace/images/test'
 dest_train = 'D:/TFODCourse/Tensorflow/workspace/images/train'
 
-files_below = os.listdir(source_below)
-for file in files_below:
-    shutil.move(f"{source_below}/{file}", dest_test)
+name = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+for i in range(26):
+    os.chdir(os.path.join(source_below,name[i]))
+    files_below = os.listdir()
+    for file in files_below:
+        shutil.copy(f"{os.path.join(source_below,name[i])}/{file}", dest_train)
+for i in range(26):
+    os.chdir(os.path.join(source_left, name[i]))
+    files_left = os.listdir()
+    for file in files_left:
+        shutil.copy(f"{os.path.join(source_left, name[i])}/{file}", dest_train)
+for i in range(26):
+    os.chdir(os.path.join(source_right, name[i]))
+    files_right = os.listdir()
+    for file in files_right:
+        shutil.copy(f"{os.path.join(source_right, name[i])}/{file}", dest_train)
+for i in range(26):
+    os.chdir(os.path.join(source_front, name[i]))
+    files_front = os.listdir()
+    for file in files_front:
+        shutil.copy(f"{os.path.join(source_front, name[i])}/{file}", dest_test)
 
-files_left = os.listdir(source_left)
-for file in files_left:
-    shutil.move(f"{source_left}/{file}", dest_test)
 
-files_right = os.listdir(source_right)
-for file in files_right:
-    shutil.move(f"{source_right}/{file}", dest_test)
-
-files_front = os.listdir(source_front)
-for file in files_front:
-    shutil.move(f"{source_front}/{file}", dest_train)
